e3e9f4f35447276fdecadbd3ab9710b2baf3c4f7
Filip Blaauw <filipbl@gmail.com>
2018-04-12 18:48:57 +0200

    Translation fixes

--- resources/lang/nb/dashboard.php
+++ resources/lang/nb/dashboard.php
@@ -1,8 +1,8 @@
 <?php return array (
   'dashboard' => 'Dashboard',
   'month' => 'denne måneden',
-  'period_registered_user' => 'Registrerte brukere :period ',
-  'registered_user' => 'Registered User',
+  'period_registered_user' => 'Registrerte brukere :period',
+  'registered_user' => 'Registrert bruker',
   'today' => 'i dag',
   'total' => 'totalt',
   'week' => 'denne uken',
