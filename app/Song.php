9862a129511eceb3b59799d754d86091e61f95cb
Filip Blaauw <filipbl@gmail.com>
2018-05-06 18:34:20 +0200

    api return cleanup

--- app/Song.php
+++ app/Song.php
@@ -27,7 +27,6 @@ class Song extends Model
 
         return $q ->where('artist', 'like', '%'.$keyword.'%')
                   ->orWhere('title', 'like', '%'.$keyword.'%')
-                  //->orWhere('genre->name', 'like', '%'.$keyword.'%')
                   ->orWhere('chords', 'like', '%'.$keyword.'%');
     }
 
